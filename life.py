5ade033f7493265e387949e36071e74444cd2ae7
Albert Fit <albert.ioan.fit@gmail.com>
2023-10-20 19:34:56 +0100

    first commit

---
+++ life.py
@@ -0,0 +1,9 @@
+theAnswer = 42
+
+def quote_marvin():
+    print('I\'ve calculated your chance of survival, but I don\'t think you\'ll like it.')
+
+import math
+
+def square_root(a):
+    return math.sqrt(a)
